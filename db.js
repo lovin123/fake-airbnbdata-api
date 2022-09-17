d5df6a5bd6bf613d7e8502cf49d18f6b549f5a9d
lovin123 <202112102@daiict.ac.in>
2022-09-17 22:08:06 +0530

    second commit

---
+++ db.js
@@ -0,0 +1,34 @@
+const home1 = require("./data/home1");
+const home2 = require("./data/home2");
+const search = require("./data/search");
+
+module.exports = {
+  home1,
+  home2,
+  search
+}
+
+
+// const items = require("./data/items");
+
+// module.exports = () => {
+
+//   var tags = items.map((i) => i.tags);
+//   tags = [].concat.apply([], tags);
+//   tags = [...new Set(tags)];
+
+ 
+
+//   var brands = items.map((i) => i.manufacturer);
+//   brands = [...new Set(brands)];
+
+//   var itemTypes = items.map((i) => i.itemType);
+//   itemTypes = [...new Set(itemTypes)];
+
+//   return {
+//     items,
+//     tags,
+//     brands,
+//     itemTypes,
+//   };
+// };
